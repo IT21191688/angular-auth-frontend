5dd78b388a085325219dd01f79cba1e9c57e938e
IT21191688 <IT21191688@my.sliit.lk>
2024-01-21 16:38:37 +0530

    signup

--- src/app/modules/security/components/signup/signup.component.ts
+++ src/app/modules/security/components/signup/signup.component.ts
@@ -1,10 +1,20 @@
+// signup.component.ts
+
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.sass']
+  styleUrls: ['./signup.component.css'],
 })
 export class SignupComponent {
+  user = {
+    firstName: '',
+    email: '',
+    password: '',
+  };
 
+  onSubmit() {
+    console.log('User submitted:', this.user);
+  }
 }
