24691c8c0779c239a9d15225a2e3a3624b33cafc
IT21191688 <IT21191688@my.sliit.lk>
2024-01-21 14:14:59 +0530

    init modules and components

---
+++ src/app/modules/doctor/components/doctor-verification/doctor-verification.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-doctor-verification',
+  templateUrl: './doctor-verification.component.html',
+  styleUrls: ['./doctor-verification.component.sass']
+})
+export class DoctorVerificationComponent {
+
+}
